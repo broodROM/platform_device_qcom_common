CyanogenMod/android_device_qcom_common

qcom/common: add dtbtool
This is a combination of 3 commits.

Author: David Ng <dave@codeaurora.org>
Date:   Fri Jul 27 23:52:11 2012 +0000

    dtbtool: Initial revision of table of device tree tool

    A tool to generate an image of table of device tree.  The image
    contains a header, table of device tree indices followed by the
    list of device tree blobs.

    Refer to dtbtool.txt for more information.

    Change-Id: Ib0d5f974cdb8a8fc341f01b5d086a5285b1d7dc5

Author: David Ng <dave@codeaurora.org>
Date:   Thu Aug 9 23:56:41 2012 +0000

    dtbtool: Add support for array of ID's in qcom,msm-id tag

    Allow multiple chipset/platform/soc_rev triplets in
    qcom,msm-id tag.  This allows one DTS/DTB file to be used
    for multiple chipset/platform/soc_rev variants.

    Preliminary version - needs error handling in triplet
    parsing and general code cleanup.

    Change-Id: Ic3d018c0667d1ec7189d6e14b3378effda00d781

Author: Duy Truong <dtruong@codeaurora.org>
Date:   Sun Feb 10 14:35:11 2013 +0000

    Update copyright to The Linux Foundation

    Change-Id: Id084b09bf3a083ea5c0389900bbeb5ef149b3108

Change-Id: I9efb55e1fc76e2e4e850d3853ff287715c43eee1

dtbtool/Android.mk
@@ -1,0 +1,16 @@
+ifeq ($(BOARD_KERNEL_SEPARATED_DT),true)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	dtbtool.c
+
+LOCAL_CFLAGS += \
+	-Wall
+
+LOCAL_MODULE := dtbTool
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_EXECUTABLE)
+endif

broodROM/platform_device_qcom_common

dtbtool/Android.mk
@@ -1,0 +1,17 @@
+ifeq ($(BOARD_KERNEL_SEPARATED_DT),true)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	dtbtool.c
+
+LOCAL_CFLAGS += \
+	-Wall
+
+## Hybrid v1/v2 dtbTool. Use a different name to avoid conflicts with copies in device repos
+LOCAL_MODULE := dtbToolCM
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_EXECUTABLE)
+endif